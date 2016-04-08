dfd5b676a231e7063eadb60c32cb906d7252fc5a
Nirbhay Modhe <nirbhaym@cse.iitk.ac.in>
2016-04-08 20:58:36 +0530

    Minor tweaks, comments added for better readability.

--- fd_colorize.py
+++ fd_colorize.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 
 #--- Global declarations
 NumClusters = 20
-PatchSize = 27
+PatchSize = 15
 PatchStride = PatchSize
 
 #########################################################################
@@ -20,28 +20,28 @@ PatchStride = PatchSize
 #########################################################################
 
 def PatchExtractor(image,psize,stride=None):
-    assert(psize%2 == 1) # Patch size must be odd
+    assert(psize%2 == 1) #--- Patch size must be odd
 
     patches = []
-    # ytrain = np.zeros( (image.shape[0] - (1+psize),image.shape[1] - (1+psize)) )
     utrain = np.zeros(( (1+image.shape[0]-psize)//stride, (1+image.shape[1]-psize)//stride ))
     vtrain = np.zeros(( (1+image.shape[0]-psize)//stride, (1+image.shape[1]-psize)//stride ))
     if stride is None:
         stride = psize
 
-    dim = 0 # Use only Y (Luminance)
+    dim = 0 #--- Use only Y (Luminance)
     
-    # for dim in range(image.shape[2]):
+    #--- Iterates over all possible patch centers in the image which give a full patch
     for ny in range( (1+image.shape[1]-psize)//stride ):
         for nx in range( (1+image.shape[0]-psize)//stride ):
-            # patches.append(image[nx*stride:(nx*stride)+psize,ny*stride:(ny*stride)+psize,dim])
+            #--- Get patch for current center pixel:
             patch = image[nx*stride:(nx*stride)+psize,ny*stride:(ny*stride)+psize,dim]
-            # print patch.reshape((-1,1)).shape
             patches.append( patch.reshape((1,-1)))
+            
+            #--- Get UV values for current center pixel:
             utrain[nx,ny] = image[(nx*stride)+(psize//2),(ny*stride)+(psize//2),1]
             vtrain[nx,ny] = image[(nx*stride)+(psize//2),(ny*stride)+(psize//2),2]
 
-    # UV Values to train for regression
+    #--- UV Values to train for regression
     utrain = utrain.reshape((-1,1))
     vtrain = vtrain.reshape((-1,1))
     return np.vstack(patches),utrain,vtrain
@@ -54,9 +54,6 @@ def PatchExtractor(image,psize,stride=None):
 #########################################################################
 
 def TrainKmeansAndRegression(patches,u_vals,v_vals):
-    # global PatchSize
-    # global PatchStride
-
     # Model declarations
     lrmodel = LinearRegression(n_jobs=-1)
     kmodel = KMeans(n_clusters=NumClusters,n_jobs=-1)
@@ -64,25 +61,18 @@ def TrainKmeansAndRegression(patches,u_vals,v_vals):
     labels = kmodel.fit_predict(patches)
     print "KMeans clustering completed."
 
-    # mean_patch = np.zeros((PatchSize,PatchSize))
     u_reg_models = []
     v_reg_models = []
 
     for i,pt in enumerate(kmodel.cluster_centers_):
-        # mean_patch = pt.reshape(PatchSize,PatchSize)
-        # mean_patch = np.uint8(mean_patch)
-        # mean_patch = cv2.cvtColor(mean_patch,cv2.COLOR_GRAY2BGR)
-        # print mean_patch
-        # cv2.imshow("Mean patch",mean_patch)
-        # cv2.waitKey(0)
         nearest_points = patches[labels == i,]
         u_regress = u_vals[labels == i]
         v_regress = v_vals[labels == i]
 
-        # To verify that average of the spliced points is actually the mean_patch, use this line:
-        # print "Average: ",np.average(nearest_points,axis=0),"\nvs mean:",pt
-
+        #--- To verify that average of the spliced points is actually the mean_patch, use this line:
+        #--- print "Average: ",np.average(nearest_points,axis=0),"\nvs mean:",pt
         # print "nearest_points:",nearest_points.shape
+
         lrmodel.fit(nearest_points,u_regress)
         u_reg_models.append( copy.deepcopy(lrmodel) )
         lrmodel.fit(nearest_points,v_regress)
@@ -93,6 +83,14 @@ def TrainKmeansAndRegression(patches,u_vals,v_vals):
         sys.stdout.flush()
     return kmodel,u_reg_models,v_reg_models
 
+#########################################################################
+# AssignColor:
+#   Takes as input the target gray-scale image, the 
+#   trained k-means model and UV regression model lists,
+#   and the patch size.
+#   Returns final colorized image.
+#########################################################################
+
 def AssignColor(image,kmodel,u_reg_models,v_reg_models,psize): #Image should be YUV
     p_by2 = psize//2
     patches = []
@@ -102,23 +100,16 @@ def AssignColor(image,kmodel,u_reg_models,v_reg_models,psize): #Image should be 
             patch = image[ px-p_by2:1+px+p_by2, py-p_by2:1+py+p_by2, 0]
             patches.append( patch.reshape((1,-1)) )
             patch_centers.append( [px,py] )
-    
+
     patches = np.vstack(patches)
-    # print patches.shape
     mean_labels = kmodel.predict( patches )
-    # u_val = u_reg_models[mean_label].predict()
 
     for i,pt in enumerate(kmodel.cluster_centers_):
+        #--- The indices for the patches which belong to current cluster
         indices = [mean_labels == i]
-        # print "Len of patch_centers:",len(patch_centers)
-        # print "Shape of indices:",indices.shape
-        # print indices
         for ind in range(len(patch_centers)):
             if indices[0][ind] is False:
-                # print 'False'
                 continue
-            # print "ind:",ind
-            # print "patch_centers:", patch_centers[ind]
             # u_patch = image[ px-p_by2:1+px+p_by2, py-p_by2:1+py+p_by2, 0]
             # v_patch = image[ px-p_by2:1+px+p_by2, py-p_by2:1+py+p_by2, 0]
             u_val = u_reg_models[i].predict( patches[ind].reshape(1,-1) )
@@ -128,12 +119,13 @@ def AssignColor(image,kmodel,u_reg_models,v_reg_models,psize): #Image should be 
             image[px,py,1] = u_val
             image[px,py,2] = v_val
             
+            #--- Progress reporting section
             if ind%10000 == 0:
                 string = "\rProgress: %5.2f " % ( 100.0*ind/len(patch_centers) )
                 string += " Last u,v assigned: %3.2f %3.2f" % (u_val,v_val)
                 sys.stdout.write(string)
                 sys.stdout.flush()
-        print "Phase completed: ", i+1," out of ",len(kmodel.cluster_centers_)
+        print "\nPhase completed: ", i+1," out of ",len(kmodel.cluster_centers_)
     
     return np.uint8(image)
 
@@ -141,7 +133,7 @@ if __name__ == "__main__":
     S = cv2.imread("img_color.jpg") # Source color image
     T = cv2.imread("img_gray.jpg",0) # Target gray image
 
-    # YUV Conversion
+    #--- YUV Conversion
     S = cv2.cvtColor(cv2.imread("img_color.jpg"),cv2.COLOR_BGR2YUV) 
     T = cv2.cvtColor(cv2.imread("img_gray.jpg",0),cv2.COLOR_GRAY2BGR) 
     T = cv2.cvtColor(T,cv2.COLOR_BGR2YUV)
@@ -161,10 +153,11 @@ if __name__ == "__main__":
         (kmodel,u_reg_models,v_reg_models) = pickle.load( open( train_filename+".p", "rb" ) )
         print "Successfully loaded pickle file:",train_filename
     except IOError:
-        print "Pickle file not found:",patches_filename,"\n Training models..."
+        print "Pickle file not found:",train_filename,"\n Training models..."
         kmodel,u_reg_models,v_reg_models = TrainKmeansAndRegression(patches,u_vals,v_vals)
         pickle.dump( (kmodel,u_reg_models,v_reg_models), open( train_filename+".p", "wb" ),protocol=pickle.HIGHEST_PROTOCOL )
 
+    #--- Colorization step
     output_yuv = AssignColor(T,kmodel,u_reg_models,v_reg_models,PatchSize)
 
     output_image = cv2.cvtColor(output_yuv,cv2.COLOR_YUV2BGR)
