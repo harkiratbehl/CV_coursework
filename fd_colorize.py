30118b214e2224a739694d9977e959774a71217e
Nirbhay Modhe <nirbhaym@cse.iitk.ac.in>
2016-04-08 15:04:10 +0530

    Functionalized K-means and regression. Will now add pickle support in next commit.

--- fd_colorize.py
+++ fd_colorize.py
@@ -6,8 +6,18 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 
+#--- Global declarations
+NumClusters = 3
 PatchSize = 27
 PatchStride = PatchSize*10
+
+#########################################################################
+# Patch Extractor:
+#   Takes as input a single image and returns a 
+#   list of patches (each resized to 1-D) using
+#   PatchSize and PatchStride.
+#   Note: Only Luminance (Y) patches are returned.
+#########################################################################
 
 def PatchExtractor(image,psize,stride=None):
     assert(psize%2 == 1) # Patch size must be odd
@@ -35,6 +45,53 @@ def PatchExtractor(image,psize,stride=None):
     utrain = utrain.reshape((-1,1))
     vtrain = vtrain.reshape((-1,1))
     return np.vstack(patches),utrain,vtrain
+
+#########################################################################
+# TrainKmeansAndRegression:
+#   Trains the K-means clutering model and uses
+#   the U-V values of the patche centers to train
+#   regression models for each cluster center
+#########################################################################
+
+def TrainKmeansAndRegression(patches,u_vals,v_vals):
+    # global PatchSize
+    # global PatchStride
+
+    # Model declarations
+    lrmodel = LinearRegression(n_jobs=-1)
+    kmodel = KMeans(n_clusters=NumClusters,n_jobs=-1)
+
+    labels = kmodel.fit_predict(patches)
+    print "KMeans clustering completed."
+
+    # mean_patch = np.zeros((PatchSize,PatchSize))
+    u_reg_models = []
+    v_reg_models = []
+
+    for i,pt in enumerate(kmodel.cluster_centers_):
+        # mean_patch = pt.reshape(PatchSize,PatchSize)
+        # mean_patch = np.uint8(mean_patch)
+        # mean_patch = cv2.cvtColor(mean_patch,cv2.COLOR_GRAY2BGR)
+        # print mean_patch
+        # cv2.imshow("Mean patch",mean_patch)
+        # cv2.waitKey(0)
+        nearest_points = patches[labels == i,]
+        u_regress = u_vals[labels == i]
+        v_regress = v_vals[labels == i]
+
+        # To verify that average of the spliced points is actually the mean_patch, use this line:
+        # print "Average: ",np.average(nearest_points,axis=0),"\nvs mean:",pt
+
+        # print "nearest_points:",nearest_points.shape
+        lrmodel.fit(nearest_points,u_regress)
+        u_reg_models.append( copy.deepcopy(lrmodel) )
+        lrmodel.fit(nearest_points,v_regress)
+        v_reg_models.append( copy.deepcopy(lrmodel) )
+
+        string = "\rTraining regression models... %4.2f " % ( 100.0*i/len(kmodel.cluster_centers_) )
+        sys.stdout.write(string)
+        sys.stdout.flush()
+    return kmodel,u_reg_models,v_reg_models
 
 def AssignColor(image,kmodel,u_reg_models,v_reg_models,psize): #Image should be YUV
     p_by2 = psize//2
@@ -90,41 +147,14 @@ if __name__ == "__main__":
     T = cv2.cvtColor(T,cv2.COLOR_BGR2YUV)
 
     patches,u_vals,v_vals = PatchExtractor(S,PatchSize,PatchStride)
-    print "Patches shape:",patches.shape
 
-    lrmodel = LinearRegression(n_jobs=-1)
-    kmodel = KMeans(n_clusters=3,n_jobs=-1)
-    labels = kmodel.fit_predict(patches)
-    print "KMeans Model fit done!"
-
-    mean_patch = np.zeros((PatchSize,PatchSize))
-    u_reg_models = []
-    v_reg_models = []
-
-    for i,pt in enumerate(kmodel.cluster_centers_):
-        mean_patch = pt.reshape(PatchSize,PatchSize)
-        mean_patch = np.uint8(mean_patch)
-        mean_patch = cv2.cvtColor(mean_patch,cv2.COLOR_GRAY2BGR)
-        # print mean_patch
-        # cv2.imshow("Mean patch",mean_patch)
-        # cv2.waitKey(0)
-        nearest_points = patches[labels == i,]
-        u_regress = u_vals[labels == i]
-        v_regress = v_vals[labels == i]
-
-        # To verify that average of the spliced points is actually the mean_patch, use this line:
-        # print "Average: ",np.average(nearest_points,axis=0),"\nvs mean:",pt
-
-        print "nearest_points:",nearest_points.shape
-        lrmodel.fit(nearest_points,u_regress)
-        u_reg_models.append( copy.deepcopy(lrmodel) )
-        lrmodel.fit(nearest_points,v_regress)
-        v_reg_models.append( copy.deepcopy(lrmodel) )
+    kmodel,u_reg_models,v_reg_models = TrainKmeansAndRegression(patches,u_vals,v_vals)
 
     output_yuv = AssignColor(T,kmodel,u_reg_models,v_reg_models,PatchSize)
 
     output_image = cv2.cvtColor(output_yuv,cv2.COLOR_YUV2BGR)
     cv2.imwrite("ColorizedOutput.jpg",output_image)
     cv2.imshow("Final output",output_image)
+
     while cv2.waitKey(0) != 27:
         cv2.waitKey(10000)
