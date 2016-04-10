18dfb6d39ab66f5e9f4e1c9e62b556915c678d42
Bhuvesh Kumar <bhuvesh@cse.iitk.ac.in>
2016-04-10 19:27:14 +0530

    multiplecenter in patchgen

--- patch_generator.py
+++ patch_generator.py
@@ -8,8 +8,8 @@
 from dependency import *
 
 class patch_generator:
-    def __init__(self,imagefolder,psize,stride):
-        self.patches,self.u_vals,self.v_vals = self.PatchExtractor(imagefolder,psize,stride)
+    def __init__(self,imagefolder,psize,stride,CenterSize):
+        self.patches,self.u_vals,self.v_vals = self.PatchExtractor(imagefolder,psize,stride,CenterSize)
 
     def PatchExtractor(a,image,psize,stride=None):
         # print image
@@ -30,16 +30,18 @@ class patch_generator:
         for ny in range( (1+image.shape[1]-psize)//stride ):
             for nx in range( (1+image.shape[0]-psize)//stride ):
                 #--- Get patch for current center pixel:
-                # print 'hello'
                 patch = image[nx*stride:(nx*stride)+psize,ny*stride:(ny*stride)+psize,dim]
                 patches.append( patch.reshape((1,-1)))
                 
                 #--- Get UV values for current center pixel:
-                utrain[nx,ny] = image[(nx*stride)+(psize//2),(ny*stride)+(psize//2),1]
-                vtrain[nx,ny] = image[(nx*stride)+(psize//2),(ny*stride)+(psize//2),2]
+                ut = image[(nx*stride)+(psize//2) - CenterSize:(nx*stride)+(psize//2) + 1 + CenterSize,(ny*stride)+(psize//2) - CenterSize:(ny*stride)+(psize//2) + 1 + CenterSize,1]
+                ut = image[(nx*stride)+(psize//2) - CenterSize:(nx*stride)+(psize//2) + 1 + CenterSize,(ny*stride)+(psize//2) - CenterSize:(ny*stride)+(psize//2) + 1 + CenterSize,2]
+                utrain.append( ut.reshape((1,-1)) )
+                vtrain.append( ut.reshape((1,-1)) )
+                # utrain[nx,ny] = image[(nx*stride)+(psize//2),(ny*stride)+(psize//2),1]
+                # vtrain[nx,ny] = image[(nx*stride)+(psize//2),(ny*stride)+(psize//2),2]
 
         #--- UV Values to train for regression
-        utrain = utrain.reshape((-1,1))
-        vtrain = vtrain.reshape((-1,1))
-        # print 'hi'
-        return np.vstack(patches),utrain,vtrain
+        # utrain = utrain.reshape((-1,1))
+        # vtrain = vtrain.reshape((-1,1))
+        return np.vstack(patches),np.vstack(utrain),np.vstack(vtrain)
