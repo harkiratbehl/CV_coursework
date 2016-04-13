76ccd6264cd6a4c0d1470eb78529f1a9c6a31e34
Bhuvesh Kumar <bhuvesh@cse.iitk.ac.in>
2016-04-13 22:08:33 +0530

    less memory

--- AssignColor.py
+++ AssignColor.py
@@ -8,45 +8,37 @@ class AssignColor:
     def assignColor(self,image,kmodel,u_reg_models,v_reg_models,psize,CenterSize): #Image should be YUV
         CenterLen = (2*CenterSize + 1)*(2*CenterSize + 1)
         p_by2 = psize//2
+        print "Assigning Color"
         patches = []
         patch_centers = []
         patch_means =[]
+        ind = 0
+        prog = ((image.shape[0]-psize)*(image.shape[1] - psize))/(psize*psize)
         for px in range( p_by2, image.shape[0]-p_by2 ):
             for py in range( p_by2, image.shape[1]-p_by2 ):
                 patch = image[ px-p_by2:1+px+p_by2, py-p_by2:1+py+p_by2, 0]
-                patch_means.append(np.average(patch))
+               
+               # patch_means.append(np.average(patch))
                # patch = patch - np.average(patch)
-                patches.append( patch.reshape((1,-1)) )
-                patch_centers.append( [px,py] )
+                #patches.append( patch.reshape((1,-1)) )
+                #patch_centers.append( [px,py] )
+                patch = patch.reshape((1,-1))
+                patch = np.float64(patch)
+                i = kmodel.predict(patch.reshape(1,-1))
 
-        patches = np.vstack(patches)
-        mean_labels = kmodel.predict( patches )
-
-        
-        for ind in range(len(patch_centers)):#iterate over all patches(in a way all patches of this cluster centre)
+                u_val = u_reg_models[i][0].predict( patch.reshape(1,-1) )
+                v_val = v_reg_models[i][0].predict( patch.reshape(1,-1) )
             
-            # u_patch = image[ px-p_by2:1+px+p_by2, py-p_by2:1+py+p_by2, 0]
-            # v_patch = image[ px-p_by2:1+px+p_by2, py-p_by2:1+py+p_by2, 0]
-            i = mean_labels[ind]
-            #u_vals = np.zeros(CenterLen)
-            #v_vals = np.zeros(CenterLen)
-
-            # for j in range(CenterLen):
-            #     u_vals[j] = (1.0/CenterLen)*( u_reg_models[i][j].predict( patches[ind].reshape(1,-1)))# + patch_means[ind])#stores u values for all pixels predicted by this patch
-            #     v_vals[j] = (1.0/CenterLen)*( v_reg_models[i][j].predict( patches[ind].reshape(1,-1)))# + patch_means[ind])
-
-            u_val = u_reg_models[i][0].predict( patches[ind].reshape(1,-1) )
-            v_val = v_reg_models[i][0].predict( patches[ind].reshape(1,-1) )
-            
-            px,py = patch_centers[ind][0], patch_centers[ind][1]
-            image[px,py,1] = u_val
-            image[px,py,2] = v_val
-            
+                image[px,py,1] = u_val
+                image[px,py,2] = v_val
+                
             #--- Progress reporting section
-            if ind%1000 == 0:
-                string = "\rColor Assign Progress: %5.2f%%" % ( 100.0*ind/len(patch_centers))
-                sys.stdout.write(string)
-                sys.stdout.flush()
+                if ind%1000 == 0:
+                    string = "\rColor Assign Progress: %5.2f%%" % ( 100.0*ind/prog)
+                    sys.stdout.write(string)
+                    sys.stdout.flush()                
+                ind = ind + 1
+        
         img = image
         uchannel = img[:,:,1]
         uchannel = cv2.GaussianBlur(uchannel, (5,5),0)
